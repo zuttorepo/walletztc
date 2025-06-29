6b0cf594bf424b62598ab0a3759675a2f6ee27be
zuttorepo <walletzuttocoin@gmail.com>
2025-06-29 22:30:43 +0700

    Create zt_signer.py

---
+++ core/zt_signer.py
@@ -0,0 +1,6 @@
+from bitcoin import *
+
+def build_and_sign_tx(from_addr, to_addr, amount, privkey):
+    # Dummy TX (harus diganti TX builder real dari UTXO)
+    tx = f"Signed TX from {from_addr} to {to_addr} for {amount} BTC"
+    return tx
